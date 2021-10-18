3acbc0ae941584a23ceae6275cc1946016e4799f
hcy6021 <81142151+hcy6021@users.noreply.github.com>
2021-10-18 13:29:53 +0900

    TabOneScreen 수정

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,9 +8,8 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>LoLing</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
@@ -20,10 +19,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#161627'
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: '#FFFFFF'
   },
   separator: {
     marginVertical: 30,
