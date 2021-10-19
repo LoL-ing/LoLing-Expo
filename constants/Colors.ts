f7bcdad5778a2ca2b9c732fb226efffad5145b7f
narongkim <kyj0032@korea.ac.kr>
2021-10-19 22:56:04 +0900

    Homescreen 스크롤, color palette

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,3 +1,5 @@
+import { DarkTheme } from "@react-navigation/native";
+
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
@@ -10,10 +12,12 @@ export default {
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: '#C5A3FF', /*보라색*/
+    background: '#161627' /*배경색-검정에 가까움*/,
+    background2: '#23233F', /*배경색-남색*/
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  gray: '#DFDFDF',
 };
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,6 +3,9 @@ import { Pressable, ScrollView, StyleSheet, Image } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import color from '../constants/Colors'
+import navigation from '../navigation';
+import ProfileScreen from '../screens/ProfileScreen';
 
 export default function HomeScreen() {
   return (
@@ -11,22 +14,45 @@ export default function HomeScreen() {
         <Text style={styles.profiletext}>
           하아아아품
         </Text>
-      </Pressable>
-      <ScrollView contentContainerStyle={styles.container2} horizontal={true}>
+      </Pressable >
+      <View style={styles.container2}>
         <Text style={styles.title}>
           LoL-ing 친구들
         </Text>
-      </ScrollView>
-      <ScrollView contentContainerStyle={styles.container2} horizontal={true}>
+        <ScrollView horizontal={true}
+          showsHorizontalScrollIndicator={false}>
+          <Image source = {require('../assets/images/Nunu.png')} 
+          style={styles.imagestyle}>
+          </Image>
+          <Image source = {require('../assets/images/Irelia.png')} 
+          style={styles.imagestyle}>
+          </Image>
+          <Image source = {require('../assets/images/Teemo.png')} 
+          style={styles.imagestyle}>
+          </Image>
+        </ScrollView>
+      </View>
+      <View style={styles.container2}>
         <Text style={styles.title}>
-          매칭 가능한 LoL-ing 유저들
+              매칭 가능한 LoL-ing 유저들
         </Text>
-        <Image source = {require('../assets/images/Nunu.png')}>
-        </Image>
-      </ScrollView>
-    </ScrollView>
+        <ScrollView horizontal={true}
+          showsHorizontalScrollIndicator={false}>
+          <Image source = {require('../assets/images/Nunu.png')} 
+          style={styles.imagestyle}>
+          </Image>
+          <Image source = {require('../assets/images/gold.png')} 
+          style={styles.imagestyle}>
+          </Image>
+          <Image source = {require('../assets/images/diamond.png')} 
+          style={styles.imagestyle}>
+          </Image>
+        </ScrollView>
+      </View>
+   </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -35,8 +61,8 @@ const styles = StyleSheet.create({
     height: '100%',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor: '#161627',
+    //justifyContent: 'flex-start',
+    backgroundColor: color.dark.background,
     padding: 10,
   },
   profile: {
@@ -44,22 +70,26 @@ const styles = StyleSheet.create({
     width: '95%',
     backgroundColor: '#C5A3FF',
     borderRadius: 10,
-    padding: 10
+    padding: 10,
+    margin: 10,
+    tintColor: color.dark.tint,
   },
   container2: {
-    height : 150,
+    height : 200,
     width: '95%',
     borderRadius : 10,
-    backgroundColor: '#23233F',
-    padding: 10, 
+    backgroundColor: color.dark.background2, 
+    padding: 10,
+    margin: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#C5A3FF',
+    color: color.dark.text,
+    marginBottom: 35,
   },
   profiletext: {
-    fontSize: 20,
+    fontSize: 20, 
     color: '#000000'
   },
   separator: {
@@ -67,5 +97,15 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  imagestyle: {
+    height: 100,
+    width: 100,
+    marginLeft: 20,
+    marginRight: 20,
+  },
 });
+
+function createStackNavigator(arg0: { Home: () => JSX.Element; Details: any; }, arg1: { initialRouteName: string; }) {
+  throw new Error('Function not implemented.');
+}
 
