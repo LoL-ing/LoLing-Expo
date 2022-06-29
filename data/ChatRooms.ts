ad3be5256c905ad8d4e036ecd1f4abb8ba4b199f
안희웅 <heewoong.ahn@anhuiungs-MBP.Dlink>
2022-06-30 01:58:22 +0900

    DeleteFriend Update

--- data/ChatRooms.ts
+++ data/ChatRooms.ts
@@ -3,21 +3,21 @@ const ChatRooms = [
     nickname: "커피한보틀",
     profileImg: require('../assets/images/Galio.png'),
     recentMessage: "저랑 같이 한판 하시죠!",
-    numberOfMessage: 0,
+    numberOfMessage: 1001,
 }
 ,
 {
     nickname: "하아아아ㅏ아아아아아품",
     profileImg: require('../assets/images/Irelia.png'),
     recentMessage: "싫어요",
-    numberOfMessage: 222,
+    numberOfMessage: 22,
 }
 ,
 {
     nickname: "맞 겠 니 ?",
     profileImg: require('../assets/images/Nunu.png'),
     recentMessage: "무슨 챔 하신다고요?",
-    numberOfMessage: 0,
+    numberOfMessage: 999,
 }
 ,
 ];
