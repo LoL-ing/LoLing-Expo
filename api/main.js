b3e31fbfce90d00f62e0ba96f6306533bf0d89f8
summer-bonobono <fmadudid@gmail.com>
2022-07-03 23:06:44 +0900

    백엔드 주소 변경

--- api/main.js
+++ api/main.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export function api_getFriends() {
-  return axios.get('http:/172.30.1.49:8000/Friends');
+  return axios.get('http:/54.153.85.38/Friends');
 }
 
 export function api_getProfiles() {
-  return axios.get('http:/172.30.1.49:8000/Profiles');
+  return axios.get('http:/54.153.85.38/Profiles');
 }
 
 export function api_getChampions() {
-  return axios.get('http:/172.30.1.49:8000/Champions');
+  return axios.get('http:/54.153.85.38/Champions');
 }
