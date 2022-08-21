a6227e324bf0be6fa7b7f96fd29e604461c96b2c
jooyoung0309 <92634070+jooyoung0309@users.noreply.github.com>
2022-08-21 20:36:56 +0900

    layout fontscale update

--- constants/Layout.ts
+++ constants/Layout.ts
@@ -2,7 +2,7 @@ import { Dimensions, StatusBar, NativeModules } from 'react-native';
 
 const Width = Dimensions.get('screen').width;
 const Height = Dimensions.get('screen').height;
-const FontScale = Dimensions.get('screen').fontScale + 0.3;
+const FontScale = Dimensions.get('screen').fontScale + 0.2;
 const AndroidStatusBarHeight = StatusBar.currentHeight;
 const AndroidBottomBarHeight = Height - Dimensions.get('window').height - AndroidStatusBarHeight;
 
