434dfae65a06a4ad3bff0d577020708a50488054
jooyoung0309 <92634070+jooyoung0309@users.noreply.github.com>
2022-09-07 23:31:30 +0900

    0907 프론트작업 - 커뮤니티 스크린 / 더보기 스크린 에니메이션

---
+++ components/Forum.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import Colors from '../constants/Colors';
+import Layout from '../constants/Layout';
+
+import StarOn from '../assets/icons/svg/star-on.svg';
+import StarOff from '../assets/icons/svg/star-off.svg';
+
+export default function Forum(props: {
+  bookmark: boolean;
+  title: string;
+  recentPost: string;
+}) {
+  const [bookmark, setBookmark] = useState(props.bookmark);
+  return (
+    <Pressable
+      onPress={() => setBookmark(!bookmark)}
+      style={({ pressed }) => ({
+        opacity: pressed ? 0.5 : 1,
+        width: Layout.Width * 0.86,
+        height: Layout.Height * 0.03,
+        marginTop: Layout.Height * 0.03,
+        flexDirection: 'row',
+        alignItems: 'center',
+      })}
+    >
+      {bookmark ? (
+        <StarOn
+          width={Layout.Width * 0.056}
+          style={{
+            marginRight: Layout.Width * 0.044,
+          }}
+        />
+      ) : (
+        <StarOff
+          width={Layout.Width * 0.056}
+          style={{
+            marginRight: Layout.Width * 0.044,
+          }}
+        />
+      )}
+      <View
+        style={{
+          width: Layout.Width * 0.76,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{
+            color: Colors.textWhite,
+            fontSize: Layout.FontScale * 15,
+          }}
+        >
+          {props.title}
+        </Text>
+        <Text
+          style={{
+            opacity: 0.6,
+            color: Colors.textWhite,
+            fontSize: Layout.FontScale * 12,
+          }}
+        >
+          {props.recentPost}
+        </Text>
+      </View>
+    </Pressable>
+  );
+}
---
+++ data/Forums.ts
@@ -0,0 +1,12 @@
+const Forums = [
+    {
+        bookmark: true,
+        title: '자유게시판',
+        recentPost: '브실골을 위한 딜포터 1타 공략'
+    },
+];
+
+export default function getForums() {
+    return Forums;
+  }
+  
