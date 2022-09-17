740ce7b4d07bedcb329e8764a47c79c45afb3d73
nomad2569 <nomad2569@naver.com>
2022-09-17 19:38:53 +0900

    API server 변경 (local -> EC2 server)

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 // export const isLocalServer = process.env.ENV_API == 'local' ? true : false;
-export const isLocalServer = true;
+export const isLocalServer = false;
 
 export const API_URL = isLocalServer
   ? 'http://localhost:8000'
